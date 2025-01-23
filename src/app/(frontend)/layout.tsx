fcb80a736b6486f0bc4ad62e84967a8882f93ee5
yadnesh ranshevare <yranshevare2005@gmail.com>
2025-01-23 16:30:17 +0530

    move the component

--- src/app/(frontend)/layout.tsx
+++ src/app/(frontend)/layout.tsx
@@ -1,4 +1,4 @@
-import NavBar from "./component/NavBar"
+import NavBar from "@/components/NavBar"
 export default function layout({children}:Readonly<{children:React.ReactNode}>){
     return(
         <main className="font-work-sans">
